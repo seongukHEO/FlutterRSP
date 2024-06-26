36fd4dcd4e34f74983daa9886ed4fa778ee4db03
seongukHEO <heoseonguk3@gmail.com>
2024-06-27 00:19:41 +0900

    feat : dart 함수

---
+++ lib/practice/test.dart
@@ -0,0 +1,15 @@
+void main(){
+  //함수 (메서드)
+  //리턴 타입   함수명 (매개변수) {}
+
+  int num1 = 10;
+  int num2 = 20;
+
+  int sum = add(num1, num2);
+
+  print(sum);
+}
+
+int add (int num1, int num2){
+  return num1 + num2;
+}
