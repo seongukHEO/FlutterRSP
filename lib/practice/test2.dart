e948d0fa8c8b7852ed337f159ae02f51d85e87cf
seongukHEO <heoseonguk3@gmail.com>
2024-06-27 10:41:40 +0900

    feat : dart 분기문

---
+++ lib/practice/test2.dart
@@ -0,0 +1,68 @@
+void main(){
+  // 분기문과 반복문
+  //if 문
+  //switch 문
+
+
+  //if문 (근데 내가 코틀린 했을 떄랑 매우 비슷해서 굳이..?)
+  // bool seonguk = false;
+  // if(seonguk){
+  //   print("ds");
+  // }else if()
+  //
+  // else{
+  //   print("no");
+  // }
+
+
+  //switch 문
+  //switch (비교대상) {
+      //case (조건식) :
+      // default :
+  //}
+
+  // int num = 5;
+  // switch(num){
+  //   case 1 : print(num);
+  //   case 2 : print(num);
+  //   case 3 : print(num);
+  //   case 4 : print(num);
+  //   case 5 : print(num);
+  //   default: print('Not 1 ~ 5');
+  // }
+
+
+
+
+  //반복문
+
+  //for문
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
