e9b49c86117e8b2de15cc541f2181a438db6955f
seongukHEO <heoseonguk3@gmail.com>
2024-06-27 10:52:06 +0900

    feat : dart 반복문

--- lib/practice/test2.dart
+++ lib/practice/test2.dart
@@ -37,6 +37,32 @@ void main(){
   //반복문
 
   //for문
+  // for (int i = 0; i < 10; i++){
+  //   print(i);
+  // }
+  //
+  //
+  // //for - in
+  // //리스트 변수 만들기
+  // List<int> indexs = [0, 1, 2, 3, 4, 5];
+  // for(int index in indexs){
+  //   print(index);
+  // }
+
+
+
+  //while
+  bool isRunning = true;
+  int count = 0;
+
+  while(isRunning){
+    if(count > 5){
+      isRunning = false;
+    }
+
+    count++;
+    print(count);
+  }
 
 
 
